aa74e003d3f16d86c9c7999820fb0230429904ff
Mayur Dange <161877178+dangemayur@users.noreply.github.com>
2024-08-19 10:18:59 +0000

    datatype

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail = "mayurdange09@gmail.com"
+var accountPassword = "12345"
+accountCity = "Nanded"
+
+// accountId = 2 /
+
+accountEmail = "mayur@d.com"
+accountPassword = "2566666"
+accountCity = "Pune"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountCity, accountPassword])
