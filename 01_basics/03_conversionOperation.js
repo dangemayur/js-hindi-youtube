491849feb56451ba448a3f11dc5bff236e6218e5
Mayur Dange <161877178+dangemayur@users.noreply.github.com>
2024-08-20 10:03:32 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,21 @@
+let score = "33"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valuNumber);
+// console.log(valuNumber)
+
+let isLoggedIn = 1
+
+let bolleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(bolleanIsLoggedIn);
+
+let someNumber = 33
+
+let stringNumber = string(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
