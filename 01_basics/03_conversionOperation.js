ba907e080e680f84e4177c08820c57e855b3d419
Mayur Dange <161877178+dangemayur@users.noreply.github.com>
2024-08-21 13:54:57 +0000

    conversion config done

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,21 +1,45 @@
-let score = "33"
+// let score = "33"
 
 // console.log(typeof score);
 // console.log(typeof(score));
 
-let valueInNumber = Number(score)
-// console.log(typeof valuNumber);
-// console.log(valuNumber)
+// let valueInNumber = Number(score)
+// // console.log(typeof valuNumber);
+// // console.log(valuNumber)
 
-let isLoggedIn = 1
+// let isLoggedIn = 1
 
-let bolleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(bolleanIsLoggedIn);
+// let bolleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(bolleanIsLoggedIn);
 
-let someNumber = 33
+// let someNumber = 33
 
-let stringNumber = string(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let stringNumber = string(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
 
+// operations //
+
+// let value = 3
+// let negValue = -value
+
+// console.log(negValue)
+
+// let str1 = "Mayur"
+// let str2 = " Dange"
+
+// let str3 = str1 + str2
+
+// console.log(str3);
+
+// console.log(-true);
+
+// Postfix increment
+let x = 3;
+const y = x++;
+// x is 4; y is 3
+
+let x2 = 3n;
+const y2 = x2++;
+// x2 is 4n; y2 is 3n
 
