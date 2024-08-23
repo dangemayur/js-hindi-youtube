ca37d8e5a8744d4c62ddc5e5ddde4dbbbd50ea6e
Mayur Dange <161877178+dangemayur@users.noreply.github.com>
2024-08-23 16:56:53 +0000

    datattype config done

--- 01_basics/datatyps-Summary.js
+++ 01_basics/datatyps-Summary.js
@@ -4,3 +4,14 @@
  
 //  Nonprimitive 
 //  Array, Objects, functions
+
+// JavaScript is a dynamically typed language. This means that you don't have to explicitly declare the type of a variable when you create it, and a variable's type can change at runtime.
+
+let exampleVar = 42;    // Initially, exampleVar is a number
+console.log(typeof exampleVar);  // Outputs: "number"
+
+exampleVar = "Hello";   // Now, exampleVar is a string
+console.log(typeof exampleVar);  // Outputs: "string"
+
+// stack - primitive 
+// Heap (Non-Primitive)
