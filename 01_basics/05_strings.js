3173fe8e00ac6d1720e1a45a7ad7e32eec403005
Mayur Dange <161877178+dangemayur@users.noreply.github.com>
2024-09-17 05:03:47 +0000

    Initial commit

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,15 @@
+// const name = "mayur"
+// const reoCount = 50
+
+// console.log(`Hello my name is ${name} and my repo count is ${reoCount} `);
+
+const gameName = new String('Mayur')
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('a'));
+
+const url = ("https://mayurdange.com/mayur%20dange")
+console.log(url.replace('%20', '-'));
+
+console.log(url.includes('mayur'));
+
+console.log(url.split('-'));
